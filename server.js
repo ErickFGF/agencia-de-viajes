0d1fc5bf2f939c98ec7e86fa6b2ae71021c3c827
ErickFGF <0227542@up.edu.mx>
2021-11-23 11:25:55 -0700

    change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
-const cookieParse = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.json())
