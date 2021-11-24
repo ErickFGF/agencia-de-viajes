5a36201b050646dbb1f80a243735167d734a24f2
ErickFGF <0227542@up.edu.mx>
2021-11-23 19:55:30 -0700

    changes

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -20,17 +20,48 @@ const userCtrl = {
 
             await newUser.save()
 
-            const accessToken = createAccessToken({id: newUser._id})
-            const refreshToken = createRefreshToken({id: newUser._id})
+            const accesstoken = createAccessToken({id: newUser._id})
+            const refreshtoken = createRefreshToken({id: newUser._id})
 
-            res.cookie('refreshtoken', refreshToken, {
+            res.cookie('refreshtoken', refreshtoken, {
                 httpOnly: true,
                 path: '/user/refresh_token'
             })
 
-            res.json({msg: "Register Success!"})
+            res.json({accesstoken})
 
         }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    login: async(req, res) =>{
+        try {
+            const {email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+
+            res.json({accesstoken})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async(req, res) =>{
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged out."})
+        } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
@@ -43,10 +74,8 @@ const userCtrl = {
                 if(err) return res.status(400).json({msg: "Please login or register"})
                 const accesstoken = createAccessToken({id: user.id})
 
-                res.json({user, accesstoken})
+                res.json({accesstoken})
             })
-
-            //res.json({rf_token})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
