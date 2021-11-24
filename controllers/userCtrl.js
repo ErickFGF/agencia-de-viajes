b71fb226b472e449782389acc0c78f0875da2412
ErickFGF <0227542@up.edu.mx>
2021-11-23 19:27:16 -0700

    changes

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,15 +1,16 @@
 const Users = require('../models/userModel')
 const bcrypt = require ('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userCtrl = {
     register: async (req, res) =>{
         try{
-            const{name, email, password} = req.body;
+            const {name, email, password} = req.body;
 
-            const user = await Users.fiindOne({email})
+            const user = await Users.findOne({email})
             if(user) return res.status(400).json({msg: "The user already exists."})
 
-            if(password.length<6)
+            if(password.length < 6)
                 return res.status(400).json({msg: "Password needs to be at least 6 characters long."})
             
             const passwordHash = await bcrypt.hash(password, 10)
@@ -17,12 +18,47 @@ const userCtrl = {
                 name, email, password: passwordHash
             })
 
+            await newUser.save()
+
+            const accessToken = createAccessToken({id: newUser._id})
+            const refreshToken = createRefreshToken({id: newUser._id})
+
+            res.cookie('refreshtoken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+
             res.json({msg: "Register Success!"})
 
         }catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    refreshToken: (req, res) =>{
+        try {
+            const rf_token = req.cookies.refreshtoken;
+            if(!rf_token) return res.status(400).json({msg: "Please login or register"})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+                if(err) return res.status(400).json({msg: "Please login or register"})
+                const accesstoken = createAccessToken({id: user.id})
+
+                res.json({user, accesstoken})
+            })
+
+            //res.json({rf_token})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+        
     }
+}
+
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 
 module.exports = userCtrl
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,4 +4,6 @@ const userCtrl = require('../controllers/userCtrl')
 
 router.post('/register', userCtrl.register)
 
+router.get('/refresh_token', userCtrl.refreshToken)
+
 module.exports = router
